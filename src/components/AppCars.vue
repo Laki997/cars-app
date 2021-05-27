a894f4b118abaa3ce518e09d54c6c710b7b0a4a2
Lazar Kalajdzic <laki.kalajdzic@gmail.com>
2021-05-27 13:21:41 +0200

    dialog

--- src/components/AppCars.vue
+++ src/components/AppCars.vue
@@ -15,19 +15,34 @@
       <router-link :to="`/edit/${car.id}`" tag="button" class="btn btn-primary"
         >Edit</router-link
       >
-      <button class="btn btn-danger" @click="deleteCar(index)">
+      <button @click="prikaziDialog(index)" class="btn btn-danger">
         Delete
       </button>
+      <div>
+        <Dialog
+          :show="showDialog"
+          :cancel="cancel"
+          :confirm="confirm"
+          :title="title"
+          :description="description"
+        />
+      </div>
     </ul>
   </div>
 </template>
 
 <script>
+import Dialog from "../components/Dialog.vue";
 import CarService from "../service/CarService.js";
 export default {
+  components: { Dialog },
   data() {
     return {
       cars: "",
+      title: "Delete a car?",
+      description: "Are you sure you want to delete this car?",
+      showDialog: false,
+      selectedCar: "",
     };
   },
 
@@ -38,8 +53,21 @@ export default {
       this.cars.splice(index, 1);
       console.log(this.cars);
     },
-  },
 
+    prikaziDialog(index) {
+      this.showDialog = true;
+      this.selectedCar = this.cars[index];
+    },
+
+    cancel() {
+      console.log("Cancel");
+      this.showDialog = false;
+    },
+    confirm() {
+      this.showDialog = false;
+      this.deleteCar(this.cars.indexOf(this.selectedCar));
+    },
+  },
   async created() {
     this.cars = await CarService.getAll();
   },
